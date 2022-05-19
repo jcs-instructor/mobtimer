7b84b18df0e9637ec54046e5cc6b8997161e22b6
jcs-instructor <jcsilberman@gmail.com>
2022-05-19 14:22:42 +0000

    Added comments before extracting functions
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- HelloWorld/mobServer.ts
+++ HelloWorld/mobServer.ts
@@ -19,18 +19,25 @@ export class MobServer {
     wss.on("connection", (socket) => {
       socket.on("message", (message: string) => {
         const parsedMessage = JSON.parse(message);
+
         if (parsedMessage.action === "join") {
           const mobName = parsedMessage.mobName;
+          // register mob - get the existing mob or add it
           mobTimer = MobServer._mobs.get(mobName);
           if (!mobTimer) {
             mobTimer = new MobTimer();
             MobServer._mobs.set(mobName, mobTimer);
           }
+          // associate this socket with mobname
           socket.mobName = mobName;
         }
+
+        // update mobTimer state variables
         if (parsedMessage.action === "update") {
           mobTimer.durationMinutes = parsedMessage.durationMinutes || mobTimer.durationMinutes;
         }
+
+        // broadcast changed state to all sockets associated with mobname
         MobServer.broadcast(wss, socket.mobName, JSON.stringify(mobTimer.state));
       });
     });    
