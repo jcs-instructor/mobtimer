5731535c23f27457c9757daba8d385753defcb29
jcs-instructor <jcsilberman@gmail.com>
2022-06-02 16:50:09 -0400

    Create 2 mobs test
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- HelloWorld/mobWebSocketServer.test.ts
+++ HelloWorld/mobWebSocketServer.test.ts
@@ -17,23 +17,49 @@ describe("WebSocket Server", () => {
     afterAll(() => server.close());
 
     test("Create mob", async () => {
-        // Create test client
         const client = new MobWebTestSocket(`ws://localhost:${port}`);
         await waitForSocketState(client, client.OPEN);
-
         const testMessage = mobMessage.joinMessage("awesome-team");
         let responseMessage: string;
-
-
-
-        // Send client message
         client.send(testMessage);
-        // Perform assertions on the response
         client.close()
         await waitForSocketState(client, client.CLOSED);
         responseMessage = client.receivedMessages[0];
         const parsedMessage = JSON.parse(responseMessage);
+
         expect(parsedMessage).toEqual(new MobTimer("awesome-team").state);
     });
+
+    test("Create 2 mobs", async () => {
+        // todo: 2 very redundant code blocks:
+        // block 1
+        const client = new MobWebTestSocket(`ws://localhost:${port}`);
+        await waitForSocketState(client, client.OPEN);
+        const testMessage = mobMessage.joinMessage("awesome-team");
+        let responseMessage: string;
+        client.send(testMessage);
+        client.close()
+        await waitForSocketState(client, client.CLOSED);
+        responseMessage = client.receivedMessages[0];
+        const parsedMessage = JSON.parse(responseMessage);
+
+        // block 2
+        const client2 = new MobWebTestSocket(`ws://localhost:${port}`);
+        await waitForSocketState(client2, client2.OPEN);
+        const testMessage2 = mobMessage.joinMessage("good-team");
+        let responseMessage2: string;
+        client2.send(testMessage2);
+        client2.close()
+        await waitForSocketState(client2, client2.CLOSED);
+        responseMessage2 = client2.receivedMessages[0];
+        const parsedMessage2 = JSON.parse(responseMessage2);
+
+        // Assertions
+        expect(parsedMessage).toEqual(new MobTimer("awesome-team").state);
+        expect(parsedMessage2).toEqual(new MobTimer("good-team").state);
+    });
+
+
+    // todo: add tests for update, pause, and start messages
 
 });
