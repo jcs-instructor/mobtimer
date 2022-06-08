bbcdc46e265f4ac71428a6bef225bfb71a532276
Ethan Strominger <ethanstrominger2@gmail.com>
2022-06-08 17:07:10 -0400

    WIP

--- HelloWorld/mobWebMessages.ts
+++ HelloWorld/mobWebMessages.ts
@@ -1,10 +1,13 @@
 // TODO: refactor message functions so they are together and related
 
 export function joinMessage(mobName: string) {
-    return JSON.stringify({ action: "join", mobName: mobName });
+  return JSON.stringify({ action: "join", mobName: mobName });
 }
 
 export function updateMessage(durationMinutes: number) {
-    return JSON.stringify({ action: "update", value: { durationMinutes: 32 } })
+  return JSON.stringify({ action: "update", value: { durationMinutes: 32 } });
 }
 
+export function pauseMessage() {
+  return JSON.stringify({ action: "pause" });
+}
--- HelloWorld/mobWebSocketServer.test.ts
+++ HelloWorld/mobWebSocketServer.test.ts
@@ -2,59 +2,57 @@ import WebSocket from "ws";
 import { startMobServer } from "./mobWebSocketUtils";
 import { waitForSocketState } from "./webSocketUtils";
 import * as mobMessage from "./mobWebMessages";
-import { MobTimer } from "./mobTimer";
+import { MobTimer, Status } from "./mobTimer";
 import { MobWebTestSocket } from "./mobWebTestSocket";
 
 const port = 3000 + Number(process.env.JEST_WORKER_ID);
 
 describe("WebSocket Server", () => {
-    let server;
+  let server;
 
-    beforeAll(async () => {
-        server = await startMobServer(port);
-    });
+  beforeAll(async () => {
+    server = await startMobServer(port);
+  });
 
-    afterAll(() => server.close());
+  afterAll(() => server.close());
 
-    test("Create mob", async () => {
-        const testMessage = mobMessage.joinMessage("awesome-team");
-        const parsedMessage = await sendMessage(testMessage);
-        expect(parsedMessage).toEqual(new MobTimer("awesome-team").state);
-    });
+  test("Create mob", async () => {
+    const testMessage = mobMessage.joinMessage("awesome-team");
+    const parsedMessage = await sendMessage(testMessage);
+    expect(parsedMessage).toEqual(new MobTimer("awesome-team").state);
+  });
 
-    test("Create 2 mobs", async () => {
-        const testMessage = mobMessage.joinMessage("awesome-team");
-        const parsedMessage = await sendMessage(testMessage);
+  test("Create 2 mobs", async () => {
+    const testMessage = mobMessage.joinMessage("awesome-team");
+    const parsedMessage = await sendMessage(testMessage);
 
-        const testMessage2 = mobMessage.joinMessage("good-team");
-        const parsedMessage2 = await sendMessage(testMessage2);
+    const testMessage2 = mobMessage.joinMessage("good-team");
+    const parsedMessage2 = await sendMessage(testMessage2);
 
-        // Assertions
-        expect(parsedMessage).toEqual(new MobTimer("awesome-team").state);
-        expect(parsedMessage2).toEqual(new MobTimer("good-team").state);
-    });
+    // Assertions
+    expect(parsedMessage).toEqual(new MobTimer("awesome-team").state);
+    expect(parsedMessage2).toEqual(new MobTimer("good-team").state);
+  });
 
+  test("Pause timer", async () => {
+    const joinMessage = mobMessage.joinMessage("awesome-team");
+    await sendMessage(joinMessage);
+    const pauseMessage = mobMessage.pauseMessage();
+    const parsedMessage = await sendMessage(pauseMessage);
+    expect(parsedMessage.status).toEqual(Status.Paused);
+  });
 
-    test("Pause timer", async () => {
-        const joinMessage = mobMessage.joinMessage("awesome-team");
-        await sendMessage(joinMessage);
-        const pauseMessage = mobMessage.pauseMessage();
-        const parsedMessage = await sendMessage(pauseMessage);
-        expect(parsedMessage.status).toEqual(MobTimer.Pause); // const/enum
-    });
-
-    // todo: add tests for update and start messages
-
+  // todo: add tests for update and start messages
 });
 
 async function sendMessage(message: string) {
-    const client = new MobWebTestSocket(`ws://localhost:${port}`);
-    await waitForSocketState(client, client.OPEN);
-    let responseMessage: string;
-    client.send(message);
-    client.close();
-    await waitForSocketState(client, client.CLOSED);
-    responseMessage = client.receivedMessages[0];
-    const parsedMessage = JSON.parse(responseMessage);
-    return parsedMessage;
+  const client = new MobWebTestSocket(`ws://localhost:${port}`);
+  await waitForSocketState(client, client.OPEN);
+  let responseMessage: string;
+  client.send(message);
+  client.close();
+  await waitForSocketState(client, client.CLOSED);
+  responseMessage = client.receivedMessages[0];
+  const parsedMessage = JSON.parse(responseMessage);
+  return parsedMessage;
 }
