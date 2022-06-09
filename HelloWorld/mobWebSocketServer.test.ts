e9ddf661dac734a31bdf37f623d3244bd71a2932
jcs-instructor <jcsilberman@gmail.com>
2022-06-09 11:39:22 -0400

    Refactoring
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- HelloWorld/mobWebSocketServer.test.ts
+++ HelloWorld/mobWebSocketServer.test.ts
@@ -24,8 +24,11 @@ describe("WebSocket Server", () => {
   });
 
   test("Create 2 mobs", async () => {
-    const socket = await joinMob("awesome-team");
-    const socket2 = await joinMob("good-team");
+    const socket = await openSocket();
+    await socket.joinMob("awesome-team");
+    
+    const socket2 = await openSocket();
+    await socket2.joinMob("good-team");
 
     await socket.closeSocket();
     await socket2.closeSocket();
@@ -35,32 +38,11 @@ describe("WebSocket Server", () => {
   });
 
   test("Start timer", async () => {
-    const socket = await joinMob("awesome-team");
+    const socket = await openSocket();
+    await socket.joinMob("awesome-team");
     await socket.send(MobMessages.startMessage());
     await await socket.closeSocket();
     expect(socket.getLastJson().status).toEqual(Status.Running);
   });
 
-  // test("Pause timer", async () => {
-  //   const testMessage = MobMessages.joinMessage("awesome-team");
-  //   sendMessage(testMessage);
-  //   const pauseJson = MobMessages.pauseMessage();
-  //   const parsedMessage = await sendMessage(pauseJson);
-  // });
-  //   test("Pause timer", async () => {
-  //     const joinMessage = MobMessages.joinMessage("awesome-team");
-  //     await sendMessage(joinMessage);
-  //     const pauseMessage2 = MobMessages.pauseMessage();
-  //     const parsedMessage = await sendMessage(pauseMessage2);
-  //     expect(parsedMessage.status).toEqual(Status.Paused);
-  //   });
-
-  // todo: add tests for update and start messages
 });
-// todo: delete
-async function joinMob(mobName: string) {
-  const socket = await openSocket();
-  const testMessage = MobMessages.joinMessage(mobName);
-  socket.send(testMessage);
-  return socket;
-}
