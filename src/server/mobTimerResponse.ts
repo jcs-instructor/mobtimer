1dfb11c688fe03e8c4a96bd6086a3d9699a89268
jcs-instructor <jcsilberman@gmail.com>
2022-07-14 11:09:39 -0400

    Added comment for MobTimerResponse restructuring
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- src/server/mobTimerResponse.ts
+++ src/server/mobTimerResponse.ts
@@ -6,3 +6,20 @@ export type MobTimerResponse = {
   durationMinutes: number;
   secondsRemaining: number;
 };
+
+/*
+export type MobTimerResponse = {
+  actionInfo {
+  timestamp: …; 
+  action: “update”, “start”, “pause”, “expired” (push notification),
+  origin: “server” | “client”
+}
+  mobState {
+  mobName: string;
+  status: Status;
+  durationMinutes: number;
+  secondsRemaining: number; 
+}
+};
+
+*/
