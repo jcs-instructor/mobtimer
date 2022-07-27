5971ac159c61054be1788375ddb7cd2d8a444b8a
Ethan Strominger <ethanstrominger2@gmail.com>
2022-07-27 17:15:19 -0400

    WIP: Add button to test mob server from web page
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- src/server/public/message.js
+++ src/server/public/message.js
@@ -1,3 +1,11 @@
 function msg() {
     alert("Hello");
 }
+
+function joinMob() {
+    const client = await openSocket();
+    await client.joinMob(_mobName1);
+    await client.closeSocket();
+    expect(client.lastResponse.mobState).toEqual(new MobTimer(_mobName1).state);
+    expect(client.lastResponse.actionInfo.action).toEqual(Action.Join);
+}
