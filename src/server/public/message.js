b35da97d72d06463d6b5ad1ae9d6fc17cb8c6c78
jcs-instructor <jcsilberman@gmail.com>
2022-07-28 16:08:26 -0400

    WIP: Display message when mob server is connected
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- src/server/public/message.js
+++ src/server/public/message.js
@@ -1,20 +1,8 @@
 //import $ from "../../../node_modules/jquery";
 
-export function msg() {
-  alert("Hi");
-}
-
 const ws = new WebSocket("ws://localhost:3000");
 
 ws.onopen = function (event) {
-  alert("We're connected!");
-  //$(".mob-status").append("<p>Test</p>");
+  alert("Web socket is connected!");
+  // todo: Replace above alert with this (once working): $(".mob-status").append("<p>Web socket server is connected.</p>");
 };
-
-// function joinMob() {
-//     const client = await openSocket();
-//     await client.joinMob(_mobName1);
-//     await client.closeSocket();
-//     expect(client.lastResponse.mobState).toEqual(new MobTimer(_mobName1).state);
-//     expect(client.lastResponse.actionInfo.action).toEqual(Action.Join);
-// }
