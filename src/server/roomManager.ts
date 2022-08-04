fdd61dfc3819728a7b8e03d86c00502c3567ea19
Ethan Strominger <ethanstrominger2@gmail.com>
2022-08-04 16:14:58 -0400

    Refactoring
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- src/server/roomManager.ts
+++ src/server/roomManager.ts
@@ -4,76 +4,76 @@ import { MobTimer } from "../mobTimer";
 import { Action } from "./action";
 
 export class RoomManager {
-    
-    /*
+  /*
     todo:
     - Review broadcast functions
     - Review this file and mobSocketServer.ts - how do they look? anything else to move? rename?
     - Decide whether this should be a module or class
     */
 
-    private static _rooms: Map<string, Room> = new Map();
-    private static _mobTimers: Map<WebSocket, MobTimer> = new Map();
+  private static _rooms: Map<string, Room> = new Map();
+  private static _mobTimers: Map<WebSocket, MobTimer> = new Map();
 
-    static getMobTimer(mobName: string): MobTimer | undefined {
-        return RoomManager._rooms.get(mobName)?.mobTimer;
-    }
-
-    static getMobTimerFromSocket(socket: WebSocket) {
-      const mobTimer = RoomManager._mobTimers.get(socket); 
-      return mobTimer;
-    }
-    
-    static getSocketsForMob(mobName: string): Set<WebSocket> | undefined {
-        return RoomManager._rooms.get(mobName)?.sockets;
-    }
-
-    static getOrRegisterRoom(
-        wss: WebSocket.Server,
-        mobName: string,
-        socket: WebSocket
-    ) {
-        let mobTimer = RoomManager.getMobTimer(mobName);
-        if (!mobTimer) {
-            RoomManager._createRoom(mobName);
-            mobTimer = RoomManager.getMobTimer(mobName) as MobTimer;
-        }
-        RoomManager._rooms.get(mobName)?.sockets.add(socket);
-        RoomManager._mobTimers.set(socket, mobTimer);
-
-        return mobTimer;
-    }
-    
-  private static _createRoom(mobName: string) {
-    const mobTimer = new MobTimer(mobName);
-    mobTimer.expireFunc = () => RoomManager.broadcastToClients(mobTimer as MobTimer, Action.Expired);
-    RoomManager._rooms.set(mobName, { mobTimer: mobTimer, sockets: new Set<WebSocket> });
+  static getMobTimer(mobName: string): MobTimer | undefined {
+    return RoomManager._rooms.get(mobName)?.mobTimer;
   }
 
-    static broadcastToClients(
-        mobTimer: MobTimer,
-        action: Action
-      ) {
-        let response = JSON.stringify({
-          actionInfo: { action: action },
-          mobState: mobTimer.state,
-        });
-        RoomManager.broadcast(mobTimer.state.mobName, response);
-      }
-      
-      static broadcast(mobName: string, messageToClients: string) {
-        const sockets = RoomManager.getSocketsForMob(mobName);
-        if (!sockets) {
-          return;
-        }
-        sockets.forEach((socketClient: WebSocket) => {
-          socketClient.send(messageToClients);
-        });
-      }
+  static getMobTimerFromSocket(socket: WebSocket) {
+    const mobTimer = RoomManager._mobTimers.get(socket);
+    return mobTimer;
+  }
 
-      static resetRooms() {
-        RoomManager._rooms.clear();
-        RoomManager._mobTimers.clear();
-      }
+  static getSocketsForMob(mobName: string): Set<WebSocket> | undefined {
+    return RoomManager._rooms.get(mobName)?.sockets;
+  }
+
+  static getOrRegisterRoom(
+    wss: WebSocket.Server,
+    mobName: string,
+    socket: WebSocket
+  ) {
+    let mobTimer = RoomManager.getMobTimer(mobName);
+    if (!mobTimer) {
+      const room = RoomManager._createRoom(mobName, socket);
+      mobTimer = room.mobTimer;
+    }
+    RoomManager._rooms.get(mobName)?.sockets.add(socket);
+    RoomManager._mobTimers.set(socket, mobTimer);
+
+    return mobTimer;
+  }
+
+  private static _createRoom(mobName: string, socket: WebSocket) {
+    const mobTimer = new MobTimer(mobName);
+    mobTimer.expireFunc = () =>
+      RoomManager.broadcastToClients(mobTimer as MobTimer, Action.Expired);
+    const sockets = new Set<WebSocket>();
+    sockets.add(socket);
+    const room = { mobTimer, sockets };
+    RoomManager._rooms.set(mobName, room);
+    return room;
+  }
+
+  static broadcastToClients(mobTimer: MobTimer, action: Action) {
+    let response = JSON.stringify({
+      actionInfo: { action: action },
+      mobState: mobTimer.state,
+    });
+    RoomManager.broadcast(mobTimer.state.mobName, response);
+  }
+
+  static broadcast(mobName: string, messageToClients: string) {
+    const sockets = RoomManager.getSocketsForMob(mobName);
+    if (!sockets) {
+      return;
+    }
+    sockets.forEach((socketClient: WebSocket) => {
+      socketClient.send(messageToClients);
+    });
+  }
+
+  static resetRooms() {
+    RoomManager._rooms.clear();
+    RoomManager._mobTimers.clear();
+  }
 }
-      
