71e00e40a605484e7f00f151198a49e011d080ed
Ethan Strominger <ethanstrominger2@gmail.com>
2022-08-11 17:15:43 -0400

    Symbolic link from top dir to backend config files
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- link.sh
+++ link.sh
@@ -2,4 +2,3 @@ ln -s mobtimer-backend/package.json .
 ln -s mobtimer-backend/yarn.lock .
 ln -s mobtimer-backend/tsconfig.json .
 ln -s mobtimer-backend/jest.config.js .
-ln -s mobtimer-backend/.gitignore .
