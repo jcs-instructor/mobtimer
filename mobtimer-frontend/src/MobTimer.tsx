acf287540ad73c698bec8caeb9eedb7abdc4600a
jcs-instructor <jcsilberman@gmail.com>
2022-08-18 10:15:17 -0400

    Componentize MobTimer UI component
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

---
+++ mobtimer-frontend/src/MobTimer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MobTimer() {
+  return (
+    <div className="MobTimer">
+      <h2>Welcome to our amazing mob timer!</h2>
+    </div>
+  );
+}
+
+export default MobTimer;
