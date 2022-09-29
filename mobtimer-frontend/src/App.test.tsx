156c5356aefdf08877671f117200adde16446812
Ethan Strominger <ethanstrominger2@gmail.com>
2022-09-29 19:28:00 -0400

    Add test to frontend for husky

--- mobtimer-frontend/src/App.test.tsx
+++ mobtimer-frontend/src/App.test.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('A test that passes', () => {
+  console.log('This test passes');
+});
+
 // TODO: Unskip test and get working
-test('renders mob', () => {
-  console.log("rendering");
+test.skip('renders mob', () => {
   // render(<App />);
   // const element = screen.getByText("Welcome to our amazing mob timer!");
   // expect(element).toBeInTheDocument();
