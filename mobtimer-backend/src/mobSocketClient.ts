debed3067996bda320a87323e27be35bf31bd5de
Ethan Strominger <ethanstrominger2@gmail.com>
2022-10-17 13:49:03 -0400

    Made mobSocketClient the same

--- mobtimer-backend/src/mobSocketClient.ts
+++ mobtimer-backend/src/mobSocketClient.ts
@@ -1,16 +1,17 @@
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 import { waitForSocketState } from "./testUtils";
-import { joinRequest, MobTimerRequest } from "mobtimer-api";
+import { joinRequest } from "mobtimer-api";
 import { MobTimerResponse } from "mobtimer-api";
 import * as MobTimerRequests from "mobtimer-api";
 
 class MobSocketClient {
   private _responses: string[] = [];
-  webSocket: W3CWebSocket = new W3CWebSocket("ws://localhost:3000");
+  webSocket: W3CWebSocket = new W3CWebSocket("ws://localhost:0000");
 
   constructor(url: string) {
-    // this.webSocket = new W3CWebSocket(url);
+    this.webSocket = new W3CWebSocket(url);
     this.webSocket.onmessage = (message) => {
+      console.log("message woo hoo", message);
       this._responses.push(message.toString());
     };
   }
