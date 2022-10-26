59e17137b5aae65cf3aff9db1329df6e786cf0d7
Ethan Strominger <ethanstrominger2@gmail.com>
2022-10-26 18:08:48 -0400

    WIP: diagnose issues with mobtimer-api ref
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- link-mobtimer-api.sh
+++ link-mobtimer-api.sh
@@ -1,2 +1,7 @@
-ln -s $PWD/mobtimer-api/dist mobtimer-frontend/src/mobtimer-api
-ln -s $PWD/mobtimer-api/dist mobtimer-backend/src/mobtimer-api
+rm -rf mobtimer-frontend/node_modules/mobtimer-api
+rm -rf mobtimer-backend/node_modules/mobtimer-api
+rm -rf mobtimer-frontend/src/mobtimer-api
+rm -rf mobtimer-backend/src/mobtimer-api
+
+ln -s $PWD/mobtimer-api/dist mobtimer-frontend/node_modules/mobtimer-api
+ln -s $PWD/mobtimer-api/dist mobtimer-backend/node_modules/mobtimer-api
