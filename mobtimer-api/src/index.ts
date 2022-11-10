d8776ad857a33835510944498fdddbe8bafdb3bf
jcs-instructor <jcsilberman@gmail.com>
2022-11-10 11:32:10 -0500

    Refactor - remove redundant import/export in index.ts
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- mobtimer-api/src/index.ts
+++ mobtimer-api/src/index.ts
@@ -7,12 +7,8 @@ import { MobTimerRequest } from "./mobTimerRequests";
 import * as mobTimerRequests from "./mobTimerRequests";
 import { MobSocketClient } from "./mobSocketClient";
 import { waitForLastResponse, waitForSocketState } from "./testUtils";
-// todo: remove these 2 direct items:
-import { JoinRequest,  UpdateRequest, } from "./mobTimerRequests";
 
 export {
-  JoinRequest,  
-  UpdateRequest,
   waitForLastResponse, 
   waitForSocketState,
   MobSocketClient,
