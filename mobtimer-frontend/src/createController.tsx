20cf7d694163694cdb18fb93f5f81313905d5d3f
Ethan Strominger <ethanstrominger2@gmail.com>
2022-11-17 15:58:04 -0500

    WIP: submitAction
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- mobtimer-frontend/src/createController.tsx
+++ mobtimer-frontend/src/createController.tsx
@@ -2,19 +2,34 @@ import React from 'react';
 import { Status } from 'mobtimer-api';
 import { MobTimerResponses } from 'mobtimer-api';
 
-export function createController(message: { data: string; }, setLabel: React.Dispatch<React.SetStateAction<string>>) {
+export function createController(message: { data: string; }, setLabel: React.Dispatch<React.SetStateAction<string>>, submitAction: any, client: any) {
   const response = JSON.parse(message.data) as MobTimerResponses.SuccessfulResponse;
   switch (response.mobState.status) {
     case Status.Running: {
       setLabel("Pause");
+      submitAction = async (event: React.FormEvent<HTMLFormElement>) => {
+        // Preventing the page from reloading
+        event.preventDefault();
+        client.pause();
+      }
       break;
     }
     case Status.Paused: {
       setLabel("Resume");
+      submitAction = async (event: React.FormEvent<HTMLFormElement>) => {
+        // Preventing the page from reloading
+        event.preventDefault();
+        client.resume();
+      }
       break;
     }
     case Status.Ready: {
       setLabel("Start");
+      submitAction = async (event: React.FormEvent<HTMLFormElement>) => {
+        // Preventing the page from reloading
+        event.preventDefault();
+        client.resume();
+      }
     }
   };
 }
