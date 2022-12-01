3fd2a0f45eb6e09f02e6d489c16922c604b7cc95
Ethan Strominger <ethanstrominger2@gmail.com>
2022-12-01 16:25:21 -0500

    WIP: Start socket code for hyperapp
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- mobtimer-frontend-hyperapp/src/index.js
+++ mobtimer-frontend-hyperapp/src/index.js
@@ -1,13 +1,26 @@
-import './index.css';
-import { h, app } from 'hyperapp';
+import "./index.css";
+import { h, app } from "hyperapp";
+// import JoinMobForm from "./components/JoinMobForm";
+// import JoinMobHeading from "./components/JoinMobHeading";
+import { MobSocketClient } from "mobtimer-api";
+// import { waitForSocketState } from "mobtimer-api";
+// import ActionButton from "./components/ActionButton";
+// import { MobTimerResponses } from "mobtimer-api";
+// import { Status } from "mobtimer-api";
+// import * as Controller from "./controller";
+
+// todo: unhardcode port
+const port = 4001;
+const url = `ws://localhost:${port}`;
+const client = MobSocketClient.openSocketSync(url);
 
 const state = {
-  count: 0
+  count: 0,
 };
 
 const actions = {
-  down: () => state => ({ count: state.count - 1 }),
-  up: () => state => ({ count: state.count + 1 })
+  down: () => (state) => ({ count: state.count - 1 }),
+  up: () => (state) => ({ count: state.count + 1 }),
 };
 
 const view = (state, actions) => (
