0102b00f6795ef20fc1fd3abcc9bf9bb63669260
Ethan Strominger <ethanstrominger2@gmail.com>
2022-12-15 23:17:21 -0500

    Fix:Add event to function for event.preventDefault
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- mobtimer-frontend/src/components/JoinMobForm.tsx
+++ mobtimer-frontend/src/components/JoinMobForm.tsx
@@ -9,14 +9,14 @@ type FormParameters = {
 const JoinMobForm = ({ mobName, setMobName, submitJoinMobRequest }: FormParameters) => {
 
     return (
-        <form>
+        <form onSubmit={(event) => submitJoinMobRequest(event)}>
             <input
                 value={mobName}
                 onChange={(e) => setMobName(e.target.value)}
                 type="text"
                 placeholder="Enter a Mob Name"
             />
-            <button type="submit" onClick={() => submitJoinMobRequest}>Submit</button>
+            <button type="submit">Submit</button>
         </form>
     )
 }
