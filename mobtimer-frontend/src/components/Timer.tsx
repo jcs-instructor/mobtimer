ca2ffe2a9ef0af3954c0c4c9929e22b5b7949808
jcs-instructor <jcsilberman@gmail.com>
2023-01-12 10:16:28 -0500

    Refactor
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- mobtimer-frontend/src/components/Timer.tsx
+++ mobtimer-frontend/src/components/Timer.tsx
@@ -7,28 +7,14 @@ const Timer = () => {
 
     const [seconds, setSeconds] = useState(0);
 
-    // const setTheTimer = () => {
-    //     const millisecondsRemaining = frontendMobTimer.secondsRemaining * 1000;
-
-    //     console.log('Remaining time', millisecondsRemaining.toLocaleString("en-US") + " ms ("+ Math.round(millisecondsRemaining/1000).toLocaleString("en-US")+" sec.)");
-
-    //     const newSeconds = Math.abs(Math.floor((millisecondsRemaining % (1000 * 60)) / 1000));
-
-    //     return [newSeconds]
-    // }
-
     useEffect(() => {
         //Component mounted
         const interval = setInterval(() => {
-
-            // const [newSeconds] = setTheTimer();
             setSeconds(frontendMobTimer.secondsRemaining);
-
         }, 1000);
 
         //Component will unmount
         return () => { clearInterval(interval) }
-
 
     }, [seconds]);
 
