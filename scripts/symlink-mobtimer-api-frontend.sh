683a49c590c3408499bd858e46dc06b6270d4476
Ethan Strominger <ethanstrominger2@gmail.com>
2023-01-24 12:19:31 -0500

    Remove extra blank line

--- scripts/symlink-mobtimer-api-frontend.sh
+++ scripts/symlink-mobtimer-api-frontend.sh
@@ -4,4 +4,3 @@ rm -rf mobtimer-frontend/node_modules/mobtimer-api
 ln -s $PWD/mobtimer-api/dist mobtimer-frontend/node_modules/mobtimer-api
 rm -rf mobtimer-frontend/node_modules/mobtimer-api/package.json
 ln -s $PWD/mobtimer-api/package.json mobtimer-frontend/node_modules/mobtimer-api/package.json
-
