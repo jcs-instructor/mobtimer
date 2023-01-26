cf6415ec95b471968812a914dd0f44bc01e373f3
jcs-instructor <jcsilberman@gmail.com>
2023-01-26 10:28:14 -0500

    Revert some test changes
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- mobtimer-backend/test/mobClientServer.test.ts
+++ mobtimer-backend/test/mobClientServer.test.ts
@@ -110,7 +110,6 @@ describe("WebSocket Server", () => {
     const client = await openSocket(url);
     await client.joinMob(_mobName1);
     await client.start();
-    await TimeUtils.delaySeconds(0.5);
     await client.pause();
     await cleanUp(client);
     expect(client.lastSuccessfulResponse.mobState.status).toEqual(
@@ -153,19 +152,14 @@ describe("WebSocket Server", () => {
     async (durationSeconds: number) => {
       const toleranceSeconds = 0.1;
       const client = await openSocket(url);
-      await client.joinMob(_mobName1 + Date.now());
+      await client.joinMob(_mobName1);
       await client.update(TimeUtils.secondsToMinutes(durationSeconds));
       await client.start();
-      await TimeUtils.delaySeconds(durationSeconds + toleranceSeconds + 2);
+      await TimeUtils.delaySeconds(durationSeconds + toleranceSeconds);
       await cleanUp(client);
-      console.log(
-        `client.lastSuccessfulResponse: ${JSON.stringify(
-          client.lastSuccessfulResponse
-        )}`
+      expect(client.lastSuccessfulResponse.mobState.secondsRemaining).toEqual(
+        0
       );
-      expect(
-        client.lastSuccessfulResponse.mobState.secondsRemaining
-      ).toBeLessThanOrEqual(0.1);
       expect(client.lastSuccessfulResponse.actionInfo.action).toEqual(
         Action.Expired
       );
@@ -175,8 +169,8 @@ describe("WebSocket Server", () => {
     }
   );
 
-  test.skip.each([0.2])(
-    "Skip (test incorrect) - Only send expiration message once - Start timer with duration %p",
+  test.each([0.2, TimeUtils.millisecondsToSeconds(1)])(
+    "Only send expiration message once - Start timer with duration %p",
     async (durationSeconds: number) => {
       const toleranceSeconds = 0.1;
       const client = await openSocket(url);
@@ -188,11 +182,8 @@ describe("WebSocket Server", () => {
       await TimeUtils.delaySeconds(durationSeconds + toleranceSeconds);
       await client.start(); // 3rd start
       await cleanUp(client);
-      expect(
-        client.lastSuccessfulResponse.mobState.secondsRemaining
-      ).toBeCloseTo(
-        client.lastSuccessfulResponse.mobState.durationMinutes * 60,
-        2
+      expect(client.lastSuccessfulResponse.mobState.secondsRemaining).toEqual(
+        0
       );
       expect(client.lastSuccessfulResponse.actionInfo.action).toEqual(
         Action.Expired
@@ -203,8 +194,8 @@ describe("WebSocket Server", () => {
     }
   );
 
-  test.skip.each([0.2, TimeUtils.millisecondsToSeconds(1)])(
-    "Skip - test needs to be revisited.  Loop: Only send expiration message once - Start timer with duration %p",
+  test.each([0.2, TimeUtils.millisecondsToSeconds(1)])(
+    "Loop: Only send expiration message once - Start timer with duration %p",
     async (durationSeconds: number) => {
       console.log("xxxxxxxxxxxxxxxx");
       const toleranceSeconds = 0.1;
