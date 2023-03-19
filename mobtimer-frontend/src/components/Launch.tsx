0f3b75ae26d7ea3fa805a281ec22c16b78c1ca60
Ethan Strominger <ethanstrominger2@gmail.com>
2023-03-19 17:18:08 -0500

    UI: Add images

--- mobtimer-frontend/src/components/Launch.tsx
+++ mobtimer-frontend/src/components/Launch.tsx
@@ -7,6 +7,8 @@ const Launch = () => {
             <h1>A collaborative timer</h1>
             <h2>for your mob team</h2>
             <JoinMobForm />
+            <img style={{ width: "285px", height: "384px", bottom: "-65px", right: "377px", display: "block", position: "absolute" }} src="./images/womansitting.svg" alt="" />
+            <img style={{ width: "285px", height: "384px", bottom: "65px", right: "80px", display: "block", position: "absolute" }} src="./images/mansitting.svg" alt="" />
         </div>
     )
 }
