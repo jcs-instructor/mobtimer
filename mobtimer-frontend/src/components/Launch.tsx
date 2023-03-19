e23630ed99050bdbdb4755bb19aef9d3c48c7670
Ethan Strominger <ethanstrominger2@gmail.com>
2023-03-19 17:38:02 -0500

    More info at bottom

--- mobtimer-frontend/src/components/Launch.tsx
+++ mobtimer-frontend/src/components/Launch.tsx
@@ -4,17 +4,28 @@ import './Launch.css';
 
 const Launch = () => {
     return (
-        <div className="LaunchStyle">
-            <h1>A collaborative timer</h1>
-            <h2>for your mob team</h2>
-            <JoinMobForm />
-            <p><a href="https://www.google.com/search?q=mob+programming" rel="noreferrer">
-                Learn more about mob programming
-            </a>
-            </p>
-            <img style={{ width: "285px", height: "384px", bottom: "-65px", right: "377px", display: "block", position: "absolute" }} src="./images/womansitting.svg" alt="" />
-            <img style={{ width: "285px", height: "384px", bottom: "65px", right: "80px", display: "block", position: "absolute" }} src="./images/mansitting.svg" alt="" />
+        <div>
+            <div className="LaunchStyle">
+                <h1>A collaborative timer</h1>
+                <h2>for your mob team</h2>
+                <JoinMobForm />
+                <p><a href="https://www.google.com/search?q=mob+programming" rel="noreferrer">
+                    Learn more about mob programming
+                </a>
+                </p>
+                <img style={{ width: "285px", height: "384px", bottom: "-65px", right: "377px", display: "block", position: "absolute" }} src="./images/womansitting.svg" alt="" />
+                <img style={{ width: "285px", height: "384px", bottom: "65px", right: "80px", display: "block", position: "absolute" }} src="./images/mansitting.svg" alt="" />
 
+            </div>
+            <div className="LaunchAdStyle">
+                <h1>
+                    It works acros all your devices
+                </h1>
+                <br />
+                <h1>
+                    ...and more information to entice.
+                </h1>
+            </div>
         </div>
     )
 }
