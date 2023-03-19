287f8b97209b4c0fd0239a8cff519fcbb9d3089a
Ethan Strominger <ethanstrominger2@gmail.com>
2023-03-19 17:26:26 -0500

    Add link to learn more about mob programming

--- mobtimer-frontend/src/components/Launch.tsx
+++ mobtimer-frontend/src/components/Launch.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import JoinMobForm from "./JoinMobForm";
 import './Launch.css';
 
@@ -7,8 +8,13 @@ const Launch = () => {
             <h1>A collaborative timer</h1>
             <h2>for your mob team</h2>
             <JoinMobForm />
+            <p><a href="https://www.google.com/search?q=mob+programming" rel="noreferrer">
+                Learn more about mob programming
+            </a>
+            </p>
             <img style={{ width: "285px", height: "384px", bottom: "-65px", right: "377px", display: "block", position: "absolute" }} src="./images/womansitting.svg" alt="" />
             <img style={{ width: "285px", height: "384px", bottom: "65px", right: "80px", display: "block", position: "absolute" }} src="./images/mansitting.svg" alt="" />
+
         </div>
     )
 }
