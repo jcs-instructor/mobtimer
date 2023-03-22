b62ef3bc2742e16f0712e37718b1c86eba592bcd
jcs-instructor <jcsilberman@gmail.com>
2023-03-22 16:31:09 -0400

    Create clean-all (reyarns all)
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

---
+++ scripts/clean-all.sh
@@ -0,0 +1,12 @@
+# todo: make directory a parameter of reyarn.sh
+
+./scripts/reyarn.sh
+
+cd mobtimer-api
+../scripts/reyarn.sh
+
+cd ../mobtimer-backend
+../scripts/reyarn.sh
+
+cd ../mobtimer-frontend
+../scripts/reyarn.sh
