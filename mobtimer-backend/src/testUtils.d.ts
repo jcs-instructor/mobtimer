483807cc9fac3bb6f826868e27bec16cde29b89a
Ethan Strominger <ethanstrominger2@gmail.com>
2023-03-22 14:13:34 -0400

    Correct compile issues

--- mobtimer-backend/src/testUtils.d.ts
+++ mobtimer-backend/src/testUtils.d.ts
@@ -1,7 +1,14 @@
-import { MobTimerResponse } from "mobtimer-api";
-import { MobSocketClient } from "./mobSocketClient";
-export declare function waitForSocketState(socket: {
+// import { MobTimerResponses } from "mobtimer-api";
+import { MobSocketClient } from "mobtimer-api";
+export declare function waitForSocketState(
+  socket: {
     readyState: number;
-}, state: number): Promise<void>;
-export declare function waitForLastResponse(socket: MobSocketClient): Promise<void>;
-export declare function convertToMobTimerResponse(response: string): MobTimerResponse;
+  },
+  state: number
+): Promise<void>;
+export declare function waitForLastResponse(
+  socket: MobSocketClient
+): Promise<void>;
+// export declare function convertToMobTimerResponse(
+//   response: string
+// ): MobTimerResponse;
