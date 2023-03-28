1489efa56c96399c15e520fd76d2ff1955077b8d
Ethan Strominger <ethanstrominger2@gmail.com>
2023-03-28 19:02:42 -0400

    Minor change to Launch.tsx to force deploy

--- mobtimer-frontend/src/components/Launch.tsx
+++ mobtimer-frontend/src/components/Launch.tsx
@@ -6,7 +6,7 @@ const Launch = () => {
     return (
         <div>
             <div className="LaunchMain">
-                <h1>A collaborative timerd</h1>
+                <h1>A collaborative timer</h1>
                 <h2>for your mob team</h2>
                 <JoinMobForm />
                 <p><a href="https://www.google.com/search?q=mob+programming" rel="noreferrer">
