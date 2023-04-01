321033c431cfefb055648448642bf76a075d2acc
Ethan Strominger <ethanstrominger2@gmail.com>
2023-04-01 11:20:12 -0400

    Change image and its CSS

--- mobtimer-frontend/src/components/Launch.tsx
+++ mobtimer-frontend/src/components/Launch.tsx
@@ -4,17 +4,17 @@ import '../App.css';
 const Launch = () => {
     return (
         <div>
-            <div className="LaunchMain">
-                <h1>A collaborative timer</h1>
-                <h2>for your mob team</h2>
-                <JoinMobForm />
-                <p><a href="https://www.google.com/search?q=mob+programming" rel="noreferrer">
-                    Learn more about mob programming
-                </a>
-                </p>
-                <img style={{ width: "285px", height: "384px", bottom: "-65px", right: "377px", display: "block", position: "absolute" }} src="./images/womansitting.svg" alt="" />
-                <img style={{ width: "285px", height: "384px", bottom: "65px", right: "80px", display: "block", position: "absolute" }} src="./images/mansitting.svg" alt="" />
-
+            <div className="LaunchTop">
+                <div className="LaunchMain">
+                    <h1>A collaborative timer</h1>
+                    <h2>for your mob team</h2>
+                    <JoinMobForm />
+                    <p><a href="https://www.google.com/search?q=mob+programming" rel="noreferrer">
+                        Learn more about mob programming!
+                    </a>
+                    </p>
+                </div>
+                <img style={{ height: "285px" }} src="./images/mobrotation.svg" alt="" />
             </div>
             <div className="LaunchBottom">
                 <div>
