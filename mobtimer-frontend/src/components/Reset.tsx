52355e99ef23295939d28ed32870263d80355afc
jcs-instructor <jcsilberman@gmail.com>
2023-04-05 16:29:01 -0400

    WIP: Reset (cancel) timer from UI
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

---
+++ mobtimer-frontend/src/components/Reset.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { client } from '../timers';
+
+const Reset = () => {
+    
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        client.reset();
+    }
+
+    return (
+        <form onSubmit={(event) => onSubmit(event)}>
+            <button type="submit">Cancel</button> {/* ❌ */}
+        </form>
+    )
+}
+
+export default Reset
