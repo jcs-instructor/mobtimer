2a593b9de4a9443f9c6470b135f66f9c24654407
Ethan Strominger <ethanstrominger2@gmail.com>
2023-05-17 11:37:22 -0400

    Fix script errors

--- scripts/start-frontend-watch.sh
+++ scripts/start-frontend-watch.sh
@@ -1,5 +1,3 @@
-./scripts/generate-mobtimer-api-exports-no-watch.sh
-./scripts/symlink-mobtimer-api-frontend.sh
 # cd mobtimer-api
 # ./start-no-watch.sh
 sleep 2 # to make sure other components are compiled first
