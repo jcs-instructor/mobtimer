4c70ab0f8443a7e552720546be48ee3956b1f7bf
Ethan Strominger <ethanstrominger2@gmail.com>
2023-05-18 17:55:48 -0400

    WIP: update package.json

--- mobtimer-vscode/dist/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOGGLE_TIMER_COMMAND = void 0;
-exports.TOGGLE_TIMER_COMMAND = `mobtimer.toggle-timer`;
-//# sourceMappingURL=constants.js.map
