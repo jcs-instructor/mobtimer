01a46eda7c8ed673fe290f16cc393a38da1f6e69
Ethan Strominger <ethanstrominger2@gmail.com>
2023-06-07 15:13:03 -0400

    Fix error in build scripts

--- scripts/symlink-mobtimer-api-vscode.sh
+++ scripts/symlink-mobtimer-api-vscode.sh
@@ -1,7 +1,1 @@
-date
-echo "Linking mobtimer-api vscode"
-rm -rf mobtimer-vscode/node_modules/mobtimer-api
-ln -s $PWD/mobtimer-api/dist mobtimer-vscode/node_modules/mobtimer-api
-rm -rf mobtimer-vscode/node_modules/mobtimer-api/package.json
-ln -s $PWD/mobtimer-api/package.json mobtimer-vscode/node_modules/mobtimer-api/package.json
-Date > mobtimer-vscode/src/Date.txt # todo: Get this line to work on GitPod (Date.txt)
+scripts/symlink-mobtimer-api.sh mobtimer-vscode
