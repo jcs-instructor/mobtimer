92a62c8bd405eba651b52e39fc6b2c4041cb5f68
jcs-instructor <jcsilberman@gmail.com>
2023-06-21 16:55:11 -0400

    Controller.changeFrontendStatus tests
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- mobtimer-api/test/controller.test.ts
+++ mobtimer-api/test/controller.test.ts
@@ -1,18 +1,23 @@
-import { IWebSocketWrapper, MobSocketClient, Status, W3CWebSocketWrapper } from '../src';
-import { Controller } from '../src/controller';
+import {
+  IWebSocketWrapper,
+  MobSocketClient,
+  Status,
+  W3CWebSocketWrapper,
+} from "../src";
+import { Controller } from "../src/controller";
 
-    // const runningLocal = true;
-    // const url = runningLocal 
-    //     ? `ws://localhost:${process.env.REACT_APP_WEBSOCKET_PORT || "4000"}` 
-    //     : process.env.REACT_APP_WEBSOCKET_URL as string;
-    // const wrapperSocket = new W3CWebSocketWrapper(url) as IWebSocketWrapper; 
-    // // todo: test if connected and retry if not
-    // Controller.client = new MobSocketClient(wrapperSocket);
-    // const client = Controller.client;
+// const runningLocal = true;
+// const url = runningLocal
+//     ? `ws://localhost:${process.env.REACT_APP_WEBSOCKET_PORT || "4000"}`
+//     : process.env.REACT_APP_WEBSOCKET_URL as string;
+// const wrapperSocket = new W3CWebSocketWrapper(url) as IWebSocketWrapper; // todo: mock this
+// // todo: test if connected and retry if not
+// Controller.client = new MobSocketClient(wrapperSocket);
+// const client = Controller.client;
 
 test("Change frontend status from Ready to Running", () => {
-    Controller.changeFrontendStatus(Controller.frontendMobTimer, Status.Running);
-    expect(Controller.frontendMobTimer.status).toBe(Status.Running);
+  Controller.changeFrontendStatus(Controller.frontendMobTimer, Status.Running);
+  expect(Controller.frontendMobTimer.status).toBe(Status.Running);
 });
 
 test("Change frontend status from Running to Paused", () => {
@@ -21,3 +26,8 @@ test("Change frontend status from Running to Paused", () => {
   expect(Controller.frontendMobTimer.status).toBe(Status.Paused);
 });
 
+test("Change frontend status from Running to Ready", () => {
+  Controller.frontendMobTimer.start();
+  Controller.changeFrontendStatus(Controller.frontendMobTimer, Status.Ready);
+  expect(Controller.frontendMobTimer.status).toBe(Status.Ready);
+});
