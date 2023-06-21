f780891e13bae608fa8aedcb6393473d643c6e2d
Ethan Strominger <ethanstrominger2@gmail.com>
2023-06-21 16:47:59 -0400

    WIP: Test change frontend status
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- mobtimer-api/test/controller.test.ts
+++ mobtimer-api/test/controller.test.ts
@@ -1,16 +1,23 @@
 import { IWebSocketWrapper, MobSocketClient, Status, W3CWebSocketWrapper } from '../src';
 import { Controller } from '../src/controller';
 
+    // const runningLocal = true;
+    // const url = runningLocal 
+    //     ? `ws://localhost:${process.env.REACT_APP_WEBSOCKET_PORT || "4000"}` 
+    //     : process.env.REACT_APP_WEBSOCKET_URL as string;
+    // const wrapperSocket = new W3CWebSocketWrapper(url) as IWebSocketWrapper; 
+    // // todo: test if connected and retry if not
+    // Controller.client = new MobSocketClient(wrapperSocket);
+    // const client = Controller.client;
+
 test("Change frontend status from Ready to Running", () => {
-    const runningLocal = true;
-    const url = runningLocal 
-        ? `ws://localhost:${process.env.REACT_APP_WEBSOCKET_PORT || "4000"}` 
-        : process.env.REACT_APP_WEBSOCKET_URL as string;
-    const wrapperSocket = new W3CWebSocketWrapper(url) as IWebSocketWrapper; 
-    // todo: test if connected and retry if not
-    Controller.client = new MobSocketClient(wrapperSocket);
-    const client = Controller.client;
     Controller.changeFrontendStatus(Controller.frontendMobTimer, Status.Running);
     expect(Controller.frontendMobTimer.status).toBe(Status.Running);
+});
+
+test("Change frontend status from Running to Paused", () => {
+  Controller.frontendMobTimer.start();
+  Controller.changeFrontendStatus(Controller.frontendMobTimer, Status.Paused);
+  expect(Controller.frontendMobTimer.status).toBe(Status.Paused);
 });
 
