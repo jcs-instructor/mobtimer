6124ed612f26ff88c5317681de246dd2b1aec6ab
Ethan Strominger <ethanstrominger2@gmail.com>
2023-07-04 19:06:48 -0400

    frontend-apicopy-build.sh

---
+++ scripts/frontend-apicopy-build.sh
@@ -0,0 +1,11 @@
+cd mobtimer-api
+yarn build
+cd ../mobtimer-frontend
+yarn
+rm -rf node_modules/mobtimer-api
+cp -rf ../mobtimer-api/dist/*.* node_modules/mobtimer-api/*.*
+yarn build
+ls node_modules/mobtimer-api
+ls ../mobtimer-api/dist
+read -p "Press any key to resume ..."
+yarn start
