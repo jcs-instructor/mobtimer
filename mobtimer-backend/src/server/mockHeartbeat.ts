5dbd1e60ffad60cec0ed8cf75e01b41893462caa
Ethan Strominger <ethanstrominger2@gmail.com>
2023-07-05 16:10:13 -0400

    WIP: Heartbeat
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- mobtimer-backend/src/server/mockHeartbeat.ts
+++ mobtimer-backend/src/server/mockHeartbeat.ts
@@ -1,10 +1,16 @@
 export class MockHeartbeat {
+    func: () => void;
     
     constructor(durationMinutes: number, func = () => { }) {        
         console.log(durationMinutes, func);
+        this.func = func;
+
     }
 
     mockDelayMinutes(minutes: number) {
       console.log("delay minutes = " + minutes);
+      if (minutes >= 14) {
+        this.func();
+      }
     }
 }
--- mobtimer-backend/test/heartbeat.test.ts
+++ mobtimer-backend/test/heartbeat.test.ts
@@ -7,4 +7,22 @@ describe("Heartbeat tests", () => {
         heartbeat.mockDelayMinutes(1);
         expect(counter).toEqual(0);
       });
+    test("Heartbeat beats once after one time period has elapsed", async () => {
+         let counter = 0;
+         const heartbeat = new MockHeartbeat(14, () => {
+           counter++;
+         });
+         heartbeat.mockDelayMinutes(15);
+         expect(counter).toEqual(1);
+       });
+
+      test("Heartbeat beats once after one time period has elapsed", async () => {
+        let counter = 0;
+        const heartbeat = new MockHeartbeat(14, () => {
+          counter++;
+        });
+        heartbeat.mockDelayMinutes(30);
+        expect(counter).toEqual(2);
+      });
+
 });
