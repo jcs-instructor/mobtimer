ea70e8a2990a2890fb584ba8aed11412fd4088de
jcs-instructor <jcsilberman@gmail.com>
2023-07-05 16:17:59 -0400

    WIP: Heartbeat
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- mobtimer-backend/test/heartbeat.test.ts
+++ mobtimer-backend/test/heartbeat.test.ts
@@ -1,28 +1,32 @@
 import { MockHeartbeat } from "../src/server/mockHeartbeat";
 
 describe("Heartbeat tests", () => {
-    test("Heartbeat does nothing if time has not been reached", async () => {
-        let counter = 0;
-        const heartbeat = new MockHeartbeat(14, ()=> { counter++ });
-        heartbeat.mockDelayMinutes(1);
-        expect(counter).toEqual(0);
-      });
-    test("Heartbeat beats once after one time period has elapsed", async () => {
-         let counter = 0;
-         const heartbeat = new MockHeartbeat(14, () => {
-           counter++;
-         });
-         heartbeat.mockDelayMinutes(15);
-         expect(counter).toEqual(1);
-       });
 
-      test("Heartbeat beats once after one time period has elapsed", async () => {
-        let counter = 0;
-        const heartbeat = new MockHeartbeat(14, () => {
-          counter++;
-        });
-        heartbeat.mockDelayMinutes(30);
-        expect(counter).toEqual(2);
-      });
+  test("Heartbeat does nothing if time has not been reached", async () => {
+    let counter = 0;
+    const heartbeat = new MockHeartbeat(14, () => {
+      counter++;
+    });
+    heartbeat.mockDelayMinutes(1);
+    expect(counter).toEqual(0);
+  });
 
+  test("Heartbeat beats once after one time period has elapsed", async () => {
+    let counter = 0;
+    const heartbeat = new MockHeartbeat(14, () => {
+      counter++;
+    });
+    heartbeat.mockDelayMinutes(15);
+    expect(counter).toEqual(1);
+  });
+
+  test("Heartbeat beats once after one time period has elapsed", async () => {
+    let counter = 0;
+    const heartbeat = new MockHeartbeat(14, () => {
+      counter++;
+    });
+    heartbeat.mockDelayMinutes(30);
+    expect(counter).toEqual(2);
+  });
+  
 });
