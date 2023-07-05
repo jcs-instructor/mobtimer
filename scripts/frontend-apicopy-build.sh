a432a7f1e61aafa9f07c1dd2345bad7df71cf1a2
Ethan Strominger <ethanstrominger2@gmail.com>
2023-07-04 20:31:57 -0400

    frontend-apicopy-build.sh

--- scripts/frontend-apicopy-build.sh
+++ scripts/frontend-apicopy-build.sh
@@ -6,11 +6,11 @@ cd ../mobtimer-frontend
 yarn
 ls node_modules/mobtimer-api
 echo step 1
-rm -rf node_modules/mobtimer-api/
-mkdir node_modules/mobtimer-api
-echo x.txt > node_modules/mobtimer-api/x.txt
+rm node_modules/mobtimer-api/*.*
+echo Hello > node_modules/mobtimer-api/x.txt
 echo Step 2
 ls node_modules/mobtimer-api
+echo step 3
 cp ../mobtimer-api/dist/*.* node_modules/mobtimer-api/*.*
 ls ../mobtimer-api/dist
 yarn build
