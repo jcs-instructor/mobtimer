74a2fb26819bbdc7d3a59b5d595178986af995eb
jcs-instructor <jcsilberman@gmail.com>
2023-07-11 16:26:26 -0400

    Heartbeat
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- mobtimer-backend/test/heartbeat.test.ts
+++ mobtimer-backend/test/heartbeat.test.ts
@@ -4,50 +4,6 @@ import { Heartbeat } from "../src/server/heartbeat";
 jest.useFakeTimers();
 
 describe.only("Heartbeat tests", () => {
-  // More test examples:
-  // Set up: heartbeatDurationMinutes is 15, maxInactivityMinutes is 60 (higher later)
-
-  test("Restart heartbeat after inactivity timeout", async () => {
-    // An activity occurs after 120 min. (4 callbacks so far; both timers would've stopped after 60 min.; on activity, restart both timers);
-    // then another 30 min. later, expect 6 callbacks (i.e., 2 more callbacks added to the prior 4)
-    const callback = jest.fn();
-    const heartbeat = new Heartbeat(15, 60, callback);
-    heartbeat.start();
-    advanceTimersByMinutes(120);
-    heartbeat.restart();
-    advanceTimersByMinutes(30);
-    expect(callback).toBeCalledTimes(6);
-  });
-
-  test("Restart heartbeat after 3 heartbeats and let run for 2 heartbeats (with no timeout)", async () => {
-    // The heartbeat is restarted after 50 min. (3 callbacks so far prior to restart),
-    // then another 40 min. later, expect 5 callbacks (i.e., 2 more callbacks added to the prior 3)
-    const callback = jest.fn();
-    const heartbeat = new Heartbeat(15, 60, callback);
-    heartbeat.start();
-    advanceTimersByMinutes(50);
-    heartbeat.restart();
-    advanceTimersByMinutes(40);
-    expect(callback).toBeCalledTimes(5);
-  });
-
-  test("Restart heartbeat before first heartbeat", async () => {
-    const callback = jest.fn();
-    const heartbeat = new Heartbeat(15, 60, callback);
-    heartbeat.start();
-    advanceTimersByMinutes(10);
-    heartbeat.restart();
-    advanceTimersByMinutes(6);
-    expect(callback).toBeCalledTimes(0);
-  });
-
-  test("MaxInactivity: 60 min, Duration: 15 min, Delay: 120 min => Calls: 4", async () => {
-    const callback = jest.fn();
-    const heartbeat = new Heartbeat(15, 60, callback);
-    heartbeat.start();
-    advanceTimersByMinutes(120);
-    expect(callback).toBeCalledTimes(4);
-  });
 
   test.each([
     { heartbeatDurationMinutes: 14, delayMinutes: 1, expectedCallbacks: 0 },
@@ -63,6 +19,49 @@ describe.only("Heartbeat tests", () => {
       expect(callback).toBeCalledTimes(expectedCallbacks);
     }
   );
+  
+  test("Restart heartbeat before first heartbeat", async () => {
+    const callback = jest.fn();
+    const heartbeat = new Heartbeat(15, 60, callback);
+    heartbeat.start();
+    advanceTimersByMinutes(10);
+    heartbeat.restart();
+    advanceTimersByMinutes(6);
+    expect(callback).toBeCalledTimes(0);
+  });
+
+  test("Restart heartbeat after 3 heartbeats and let run for 2 heartbeats (with no timeout)", async () => {
+    // The heartbeat is restarted after 50 min. (3 callbacks so far prior to restart),
+    // then another 40 min. later, expect 5 callbacks (i.e., 2 more callbacks added to the prior 3)
+    const callback = jest.fn();
+    const heartbeat = new Heartbeat(15, 60, callback);
+    heartbeat.start();
+    advanceTimersByMinutes(50);
+    heartbeat.restart();
+    advanceTimersByMinutes(40);
+    expect(callback).toBeCalledTimes(5);
+  });
+  
+  test("Restart heartbeat after inactivity timeout", async () => {
+    // An activity occurs after 120 min. (4 callbacks so far; both timers would've stopped after 60 min.; on activity, restart both timers);
+    // then another 30 min. later, expect 6 callbacks (i.e., 2 more callbacks added to the prior 4)
+    const callback = jest.fn();
+    const heartbeat = new Heartbeat(15, 60, callback);
+    heartbeat.start();
+    advanceTimersByMinutes(120);
+    heartbeat.restart();
+    advanceTimersByMinutes(30);
+    expect(callback).toBeCalledTimes(6);
+  });
+
+  test("MaxInactivity: 60 min, Duration: 15 min, Delay: 120 min => Calls: 4", async () => {
+    const callback = jest.fn();
+    const heartbeat = new Heartbeat(15, 60, callback);
+    heartbeat.start();
+    advanceTimersByMinutes(120);
+    expect(callback).toBeCalledTimes(4);
+  });
+
 });
 
 function advanceTimersByMinutes(delayMinutes: number): number {
