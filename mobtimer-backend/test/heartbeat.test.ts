d74759c1aee8b87c2abb7a39bcfaabdf9fd49c73
Ethan Strominger <ethanstrominger2@gmail.com>
2023-07-11 16:21:46 -0400

    WIP: Heartbeat restart
    Co-authored-by: Joel Silberman <42779942+jcs-instructor@users.noreply.github.com>

--- mobtimer-backend/test/heartbeat.test.ts
+++ mobtimer-backend/test/heartbeat.test.ts
@@ -6,13 +6,12 @@ jest.useFakeTimers();
 describe.only("Heartbeat tests", () => {
   // More test examples:
   // Set up: heartbeatDurationMinutes is 15, maxInactivityMinutes is 60 (higher later)
-  //  - The heartbeat is restarted after 10 minutes, 6 minutes after that still no callback
   //  - An activity occurs after 120 min. (4 callbacks so far; both timers would've stopped after 60 min.; on activity, restart both timers);
   //      then another 30 min. later, expect 6 callbacks (i.e., 2 more callbacks added to the prior 4)
 
-  // The heartbeat is restarted after 50 min. (3 callbacks so far prior to restart), 
-  // then another 40 min. later, expect 5 callbacks (i.e., 2 more callbacks added to the prior 3)
-  test("Restart heartbeat and wait for another heartbeat", async () => {
+  test("Restart heartbeat after 3 heartbeats and let run for 2 heartbeats (with no timeout)", async () => {
+    // The heartbeat is restarted after 50 min. (3 callbacks so far prior to restart),
+    // then another 40 min. later, expect 5 callbacks (i.e., 2 more callbacks added to the prior 3)
     const callback = jest.fn();
     const heartbeat = new Heartbeat(15, 60, callback);
     heartbeat.start();
@@ -22,7 +21,7 @@ describe.only("Heartbeat tests", () => {
     expect(callback).toBeCalledTimes(5);
   });
 
-  test("Restart heartbeat", async () => {
+  test("Restart heartbeat before first heartbeat", async () => {
     const callback = jest.fn();
     const heartbeat = new Heartbeat(15, 60, callback);
     heartbeat.start();
