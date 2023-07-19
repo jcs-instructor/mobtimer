791042920638ae2cb62533385ace2427ec974046
jcs-instructor <jcsilberman@gmail.com>
2023-07-19 19:54:40 -0400

    Refactor: Extract processRawRequest method

--- mobtimer-backend/src/server/roomManager.ts
+++ mobtimer-backend/src/server/roomManager.ts
@@ -74,6 +74,11 @@ export class RoomManager {
     RoomManager._broadcast(sockets, message);
   }
 
+  static broadcastResponseToMob(mobTimerResponse: MobTimerResponses.SuccessfulResponse, mobName: string) {
+    let message = JSON.stringify(mobTimerResponse);
+    const sockets = RoomManager.getSocketsForMob(mobName);
+    RoomManager._broadcast(sockets, message);
+  }
   // todo: consider changing sockets parameter to room (from which the sockets can be retrieved), or if making totally generic (something else)
   private static _broadcast(
     sockets: Set<WebSocket> | undefined,
