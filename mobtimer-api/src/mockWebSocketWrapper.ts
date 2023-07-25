cf1b470543022c46d7fa5f645a3d13ee8eb227f2
jcs-instructor <jcsilberman@gmail.com>
2023-07-25 16:07:22 -0400

    WIP: mockWebSocketWrapper (broken, commented out)
    Co-authored-by: Ethan Strominger <ethanstrominger2@gmail.com>

--- mobtimer-api/src/mockWebSocketWrapper.ts
+++ mobtimer-api/src/mockWebSocketWrapper.ts
@@ -1,34 +1,34 @@
-import { w3cwebsocket as W3CWebSocket } from "websocket";
-import { IWebSocketWrapper } from "./iWebSocketWrapper";
+// import { w3cwebsocket as W3CWebSocket } from "websocket";
+// import { IWebSocketWrapper } from "./iWebSocketWrapper";
 
-export class MockWebSocketWrapper implements IWebSocketWrapper {
-  private _webSocket: W3CWebSocket | undefined;
+// export class MockWebSocketWrapper implements IWebSocketWrapper {
+//   private _webSocket: W3CWebSocket | undefined;
 
-  constructor() {
-    this._webSocket = undefined;
-  }
+//   constructor() {
+//     this._webSocket = undefined;
+//   }
 
-  public get socketState(): number {
-    return this._webSocket.readyState;
-  }
+//   public get socketState(): number {
+//     return this._webSocket.readyState;
+//   }
 
-  public sendMessage(message: string): void {
-    this._webSocket.send(message);
-  }
+//   public sendMessage(message: string): void {
+//     this._webSocket.send(message);
+//   }
 
-  public closeSocket(): void {
-    this._webSocket.close();
-  }
+//   public closeSocket(): void {
+//     this._webSocket.close();
+//   }
 
-  public get OPEN_CODE(): number {
-    return this._webSocket.OPEN;
-  }
+//   public get OPEN_CODE(): number {
+//     return this._webSocket.OPEN;
+//   }
 
-  public get CLOSED_CODE(): number {
-    return this._webSocket.CLOSED;
-  }
+//   public get CLOSED_CODE(): number {
+//     return this._webSocket.CLOSED;
+//   }
 
-  public set onmessageReceived(handler: (message: { data: any }) => void) {
-    this._webSocket.onmessage = handler;
-  }
-}
+//   public set onmessageReceived(handler: (message: { data: any }) => void) {
+//     this._webSocket.onmessage = handler;
+//   }
+// }
