358e7e2186d97c4ea7b732294d38bf4285bb2598
jcs-instructor <jcsilberman@gmail.com>
2023-12-06 16:45:00 -0500

    Comments: Dummy vs. Mock Client Socket
    Co-authored-by: Ethan-Strominger <ethanstrominger@users.noreply.github.com>

--- mobtimer-backend/test/mockClientSocket.ts
+++ mobtimer-backend/test/mockClientSocket.ts
@@ -5,7 +5,7 @@ export class MockClientSocket implements IClientSocket {
   
   client?: Client;
 
-  // As a mock socket, we directly call the server rather than sending a message to the server.
+  // As a mock socket, we directly call the server rather than sending a message to the server through a real socket.
   sendToServer = (message: string) => {
     backendUtils.processRequest(message, this);
   };
@@ -17,6 +17,9 @@ export class MockClientSocket implements IClientSocket {
   socketState = this.OPEN_CODE;
 
   // As a mock socket, we never receive a message from the server, so this never gets called.
+  // todo: Currently this is really a dummy not a mock. To make a mock, we could try calling 
+  // onmessageReceivedFunc directly here (defined in setSocketListener.ts), possibly passing into
+  // this class's constructor a reference to the controller (and anything else needed).
   onmessageReceived = (message: { data: any }) => {
     console.log(message);
   }
